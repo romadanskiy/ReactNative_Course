be48f00d0698b3c24508bbb79f8f0978c8b96da0
kirill <kirillaromad@mail.ru>
2022-10-06 20:58:36 +0300

    #counter - done project

--- counter-app/App.tsx
+++ counter-app/App.tsx
@@ -1,19 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Counter } from './components/Counter';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={['#0f0c29', '#302b63', '#24243e']} style={{flex: 1}}>
+      <View style={styles.container}>
+        <Counter titleFontSize={25} />
+
+        <StatusBar style='light' />
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
